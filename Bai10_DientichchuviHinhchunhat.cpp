e12c1ac6f579d7ed39090ed88e95b250b4fb47b8
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-18 08:22:46 +0700

    Add project files.

---
+++ Bai10_DientichchuviHinhchunhat.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "TINH DIEN TICH VA CHU VI HINH CHU NHAT\n";
+	double dai,rong,chuvi,dientich;
+	cout << "nhap chieu dai:"; cin >> dai;
+	cout << "nhap chieu rong:"; cin >> rong;
+	
+	chuvi = (dai + rong) * 2;
+	dientich = dai * rong;
+
+	cout << "chu vi:" << chuvi << endl;
+	cout << "dien tich:" << dientich << endl;
+	return 0;
+}
